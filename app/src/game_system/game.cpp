0316f1bda116571c377985801d5b983465d2bc40
PietroFerrario <pie.ferrario@gmail.com>
2026-01-16 21:09:13 +0100

    fixed intro message with json file feature

--- app/src/game_system/game.cpp
+++ app/src/game_system/game.cpp
@@ -4,46 +4,53 @@
 #include <algorithm>
 #include <cctype>
 
+Game::Game(IOText &io) : m_io{io} {}
 
-Game::Game(IOText& io): m_io{io}{}
-
-bool Game::isLost() const {
-    return(m_main_player.getHp()<0);
+bool Game::isLost() const
+{
+    return (m_main_player.getHp() < 0);
 }
 
-bool Game::isPlayerAlive() const{
-    return m_main_player.getHp()>0;
+bool Game::isPlayerAlive() const
+{
+    return m_main_player.getHp() > 0;
 }
 
-Player& Game::getMainPlayer(){
+Player &Game::getMainPlayer()
+{
     return m_main_player;
 }
 
-const Player& Game::getMainPlayer () const {
+const Player &Game::getMainPlayer() const
+{
     return m_main_player;
 }
 
-void Game::setMainPlayerName(){
-    std::string name {};
-    
-    do{
+void Game::setMainPlayerName()
+{
+    std::string name{};
+
+    do
+    {
         m_io.promptln(name, "Enter your name: ");
-    }while(!isValidName(name));
+    } while (!isValidName(name));
 
     m_main_player.setName(name);
 }
 
-const bool Game::isValidName(std::string_view name) const {
-    return std::ranges::all_of(name, [](char ch) {
-        return std::isalpha(ch) || std::isspace(ch);
-    });
+const bool Game::isValidName(std::string_view name) const
+{
+    return std::ranges::all_of(name, [](char ch)
+                               { return std::isalpha(ch) || std::isspace(ch); });
 }
 
-void Game::intro() {
-    m_io.println("Welcome traveler, what is your name?");
+void Game::intro()
+{
+    m_io.println(m_text.getText("intro"));
     setMainPlayerName();
 }
 
-void Game::lossMessage() const {
+void Game::lossMessage() const
+{
     m_io.print(m_text.getText("outro"));
 }
